b216e9840157191d2a4dd723906fb5c1b6ff8268
Nikita Prokopov <niki@tonsky.me>
2025-06-15 22:17:08 +0200

    Ditch SwiftUI, toggle on left click, menu on right

--- DarkModeToggle/DarkModeToggleApp.swift
+++ DarkModeToggle/DarkModeToggleApp.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import ServiceManagement
 import os.log
+import Cocoa
 
 public enum LaunchAtLogin {
     public static var isEnabled: Bool {
@@ -42,57 +43,142 @@ func setDark(value: Bool) -> Bool {
     return isDark()
 }
 
+class StatusBarController: ObservableObject {
+    private var statusBar: NSStatusBar
+    private var statusItem: NSStatusItem
+    private var menu: NSMenu
+    @Published var dark = isDark()
+    @Published var launch = LaunchAtLogin.isEnabled
+    
+    init() {
+        statusBar = NSStatusBar.system
+        statusItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
+        menu = NSMenu()
+        
+        setupStatusItem()
+        setupMenu()
+    }
+    
+    private func setupStatusItem() {
+        if let button = statusItem.button {
+            updateButtonImage()
+            button.action = #selector(statusBarButtonClicked)
+            button.target = self
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        }
+    }
+    
+    private func updateButtonImage() {
+        if let button = statusItem.button {
+            let imageName = dark ? "moon" : "sun"
+            if let image = NSImage(named: imageName) {
+                image.isTemplate = true
+                button.image = image
+            }
+        }
+    }
+    
+    @objc private func statusBarButtonClicked() {
+        guard let event = NSApp.currentEvent else { 
+            toggleDarkMode()
+            return 
+        }
+        
+        // Right-click or Control + Left-click shows menu
+        if event.type == .rightMouseUp || 
+           (event.type == .leftMouseUp && event.modifierFlags.contains(.control)) {
+            updateMenuItems()
+            statusItem.menu = menu
+            statusItem.menu?.popUp(positioning: nil, at: NSEvent.mouseLocation, in: nil)
+            statusItem.menu = nil
+        } else {
+            // Regular left-click toggles dark mode
+            toggleDarkMode()
+        }
+    }
+    
+    private func toggleDarkMode() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let newValue = setDark(value: !self.dark)
+            DispatchQueue.main.async {
+                self.dark = newValue
+                self.updateButtonImage()
+            }
+        }
+    }
+    
+    private func setupMenu() {
+        let lightItem = NSMenuItem(title: "Go Light", action: #selector(goLight), keyEquivalent: "")
+        lightItem.target = self
+        let darkItem = NSMenuItem(title: "Go Dark", action: #selector(goDark), keyEquivalent: "")
+        darkItem.target = self
+        let launchItem = NSMenuItem(title: "Launch at login", action: #selector(toggleLaunchAtLogin), keyEquivalent: "")
+        launchItem.target = self
+        let quitItem = NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "")
+        quitItem.target = self
+        
+        menu.addItem(lightItem)
+        menu.addItem(darkItem)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(launchItem)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(quitItem)
+        
+        updateMenuItems()
+    }
+    
+    private func updateMenuItems() {
+        if let lightItem = menu.item(withTitle: "Go Light") {
+            lightItem.isEnabled = dark
+        }
+        if let darkItem = menu.item(withTitle: "Go Dark") {
+            darkItem.isEnabled = !dark
+        }
+        if let launchItem = menu.item(withTitle: "Launch at login") {
+            launchItem.state = launch ? .on : .off
+        }
+    }
+    
+    @objc private func goLight() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let newValue = setDark(value: false)
+            DispatchQueue.main.async {
+                self.dark = newValue
+                self.updateButtonImage()
+                self.updateMenuItems()
+            }
+        }
+    }
+    
+    @objc private func goDark() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let newValue = setDark(value: true)
+            DispatchQueue.main.async {
+                self.dark = newValue
+                self.updateButtonImage()
+                self.updateMenuItems()
+            }
+        }
+    }
+    
+    @objc private func toggleLaunchAtLogin() {
+        launch = !launch
+        LaunchAtLogin.isEnabled = launch
+        updateMenuItems()
+    }
+    
+    @objc private func quit() {
+        NSApplication.shared.terminate(nil)
+    }
+}
+
 @main
 struct DarkModeToggleApp: App {
-    @State var dark = isDark()
-    @State var launch = LaunchAtLogin.isEnabled
+    @StateObject private var statusBarController = StatusBarController()
     
     var body: some Scene {
-        MenuBarExtra() {
-            Button() {
-                DispatchQueue.global(qos: .userInitiated).async {
-                    dark = setDark(value: false)
-                }
-            } label: {
-                HStack {
-                    Image("sun").renderingMode(.template)
-                    Text("Go Light")
-                }
-            }.disabled(!dark)
-            
-        
-            Button() {
-                DispatchQueue.global(qos: .userInitiated).async {
-                    dark = setDark(value: true)
-                }
-            } label: {
-                HStack {
-                    Image("moon").renderingMode(.template)
-                    Text("Go Dark")
-                }
-            }.disabled(dark)
-            
-            Divider()
-                        
-            Button() {
-                launch = !launch
-                LaunchAtLogin.isEnabled = launch
-            } label: {
-                HStack {
-                    if launch {
-                        Image("checkmark").renderingMode(.template)
-                    }
-                    Text("Launch at login")
-                }
-            }
-
-            Divider()
-            
-            Button("Quit") {
-               NSApplication.shared.terminate(nil)
-           }
-        } label: {
-            Image(dark ? "moon" : "sun").renderingMode(.template)
+        Settings {
+            EmptyView()
         }
     }
 }
